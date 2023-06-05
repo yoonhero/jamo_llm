c3df4ee5cdfc4dd4bc98045d33b42b7b02c116ae
yoonhero <yoonhero06@naver.com>
2023-06-05 23:09:26 +0900

    fix: Tranining Issue

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -4,7 +4,7 @@ corpus = "../tmp/512_chunk.txt"
 prefix = "corpus"
 vocab_size = 9993
 spm.SentencePieceTrainer.train(
-    f"--input={corpus} --model_prefix={prefix} --vocab_size={vocab_size + 7}" + 
+    f"--input={corpus} --model_prefix={prefix} --vocab_size={vocab_size + 7} --train_extremely_large_corpus=true" + 
     " --model_type=unigram" +
     " --max_sentence_length=999999" + # 문장 최대 길이
     " --pad_id=0 --pad_piece=<p>" + # pad (0)
