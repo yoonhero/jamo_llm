05b858467dffe7a1bb646789c0df42af3bd31338
yoonseonghyeon <yoonhero06@naver.com>
2023-06-09 12:06:26 +0000

    feat: tokenizinggit add .git add .!

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -1,12 +1,12 @@
 import sentencepiece as spm
 
-corpus = "../tmp/512_chunk.txt"
-prefix = "corpus"
+corpus = "../tmp/10gb_corpus.txt"
+prefix = "17000_tokenizer"
 vocab_size = 16993
 spm.SentencePieceTrainer.train(
-    f"--input={corpus} --model_prefix={prefix} --vocab_size={vocab_size + 7} --input_sentence_size=100000000 --shuffle_input_sentence=true --train_extremely_large_corpus=true" + 
+    f"--input={corpus} --model_prefix={prefix} --vocab_size={vocab_size + 7} --train_extremely_large_corpus=true" + 
     " --model_type=bpe" +
-    " --max_sentence_length=100000000" + # 문장 최대 길이
+    " --max_sentence_length=99999" + # 문장 최대 길이
     " --pad_id=0 --pad_piece=<p>" + # pad (0)
     " --unk_id=1 --unk_piece=<unk>" + # unknown (1)
     " --bos_id=2 --bos_piece=<s>" + # begin of sequence (2)
