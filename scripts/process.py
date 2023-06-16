b6141aeb016d01df376968d7146f09ade691dcac
yoonhero <yoonhero06@naver.com>
2023-06-16 22:32:11 +0900

    feat: Preprocessing

--- scripts/process.py
+++ scripts/process.py
@@ -10,42 +10,15 @@ import os
 import re
 import json
 import zipfile
-import pandas as pd 
+import pandas as pd
+import regex
 
 from process_utils import read_text_from_txt, read_text_from_xml, preprocess_news
-
-cleaning_first_patterns = [
-  r"\[\*[^\]]+\]",
-  r"~~[^~]+~~"
-]
-cleaning_first_patterns = [re.compile(pattern, re.IGNORECASE | re.MULTILINE) for pattern in cleaning_first_patterns]
-
-cleaning_patterns = [
-  r"\([^\)]+\)",
-  r'\[[^]]*\]'
-]
-cleaning_patterns = [re.compile(pattern, re.IGNORECASE | re.MULTILINE) for pattern in cleaning_patterns]
-
-# \n -> 띄어쓰기 
-# \' -> '
-replace_patterns = {
-    '\\n': "\n",
-    "\\'": "'"
-}
+from text_preprocessing import TextPreprocessing
 
 
-def clean_text(text):
-  for regex in cleaning_first_patterns:
-    text = re.sub(regex, "", text)
-  for regex in cleaning_patterns:
-    text = re.sub(regex, "", text)
-  for k, v in replace_patterns:
-    text = text.replace(k, v)
-  return text
-
-
-cpu_cores = os.cpu_count() 
-
+cpu_cores = os.cpu_count()
+clean_text = lambda x: TextPreprocessing.preprocess(x)
 
 class Preprocess():
     def __init__(self, dataset_folder: Path):
@@ -55,17 +28,18 @@ class Preprocess():
         self.target_files = glob.glob(str(dataset_folder/"*"))
         self.total_files = len(self.target_files)
 
-    def read(self):
+    def read(self, file_dir: str):
         return NotImplemented
 
     def write(self, text):
         mode = "a" if self.result_dir.exists() else "w"
         with open(str(self.result_dir), mode, encoding="utf-8") as f:
-            f.write(f"<s> {text} </s>\n")
+            f.write(f"{text}\n")
 
     def multiprocessing(self):
         pool = Pool(cpu_cores-1)
 
+        print(len(self.target_files))
         with tqdm.tqdm(total=self.total_files) as pbar:
             for _ in tqdm.tqdm(pool.imap_unordered(self.read, self.target_files)):
                 pbar.update()
@@ -76,7 +50,6 @@ class Preprocess():
     def normal(self):
         for file_dir in self.target_files:
             self.read(file_dir)
-
 
     def __str__(self):
         return f"Total File: {self.total_files}"
@@ -127,11 +100,11 @@ class EnormousBookCorpus(Preprocess):
     def __init__(self):
         ## if zipped
         ## self.unzip("../../dataset/029.대규모 구매도서 기반 한국어 말뭉치 데이터/01.데이터/1.Training/라벨링데이터/TL_unscramble.zip")
-        dataset_folder = Path("../../dataset/029.대규모 구매도서 기반 한국어 말뭉치 데이터/01.데이터/malmungchi/")
-        self.result_dir = Path("../../dataset/processed/corpus.txt")
+        dataset_folder = Path("/Volumes/T7/dataset/AIHUB/029.대규모 구매도서 기반 한국어 말뭉치 데이터/01.데이터/1.Training/라벨링데이터/TL_unscramble")
+        self.result_dir = Path("/Volumes/T7/dataset/processed/corpus.txt")
         self.result_dir.parent.mkdir(exist_ok=True)
 
-        self.target_files = glob.glob(str(dataset_folder/"*"/"*"/"*.json"))
+        self.target_files = glob.glob(str(dataset_folder/"*"/"*.json"))
         self.total_files = len(self.target_files)
 
     def unzip(self, zip_dir):
@@ -151,10 +124,11 @@ class EnormousBookCorpus(Preprocess):
         text = ""
         for paragraph in paragraphs:
             sentences = paragraph["sentences"]
-            text += " ".join([clean_text(data["text"]) for data in sentences])
-        text = text.strip()
+            text += " ".join([data["text"] if data["word_count"] >= 9 else "" for data in sentences])
+
+        text = clean_text(text).strip()
         if len(text) < 200:
-            return 
+            return
 
         self.write(text)
 
@@ -163,7 +137,7 @@ class EnormousBookCorpus(Preprocess):
 #### Total 5.78GB in 40s
 class Expertise(Preprocess):
     def __init__(self):
-        dataset_folder = Path("../../dataset/전문분야 말뭉치/corpus/training_논문")
+        dataset_folder = Path("../../dataset/전문분야 말뭉치/corpus/")
         super().__init__(dataset_folder)
 
     def read(self, file_dir):
@@ -173,8 +147,8 @@ class Expertise(Preprocess):
 
         for data in datas:
             rows = data["rows"]
-            text = " ".join([clean_text(row["text"]) for row in rows])
-            text = text.strip()
+            text = " ".join([row["text"] for row in rows])
+            text = clean_text(text).strip()
 
             if len(text) < 200:
                 continue 
@@ -205,6 +179,28 @@ class PaperSumary(Preprocess):
 
             self.write(text)
 
+class Essay(Preprocess):
+    def __init__(self):
+        dataset_folder = Path("/Volumes/T7/dataset/AIHUB/024.에세이 글 평가 데이터/01.데이터/1.Training/라벨링데이터/")
+        self.result_dir = Path("/Volumes/T7/dataset/processed/corpus.txt")
+        self.result_dir.parent.mkdir(exist_ok=True)
+
+        self.target_files = glob.glob(str(dataset_folder/"*"/"*.json"))
+        self.total_files = len(self.target_files)
+    def read(self, file_dir):
+        if "INFO" in file_dir: return
+        with open(file_dir, "r", encoding="utf-8") as f:
+            jsonString = json.load(f)
+        paragraphs = jsonString["paragraph"]
+
+        text =  " ".join([data["paragraph_txt"] for data in paragraphs])
+        text = text.replace("#@문장구분#", "")
+
+        text = clean_text(text).strip()
+        if len(text) < 200:
+            return
+
+        self.write(text)
 
 ##### NAMUU ######
 ##### 565293 rows ######
@@ -249,13 +245,20 @@ class NAMU(Preprocess):
             self.process_namu(data)
 
 
+
 if __name__ == "__main__":
-    process1 = Malmunchi_book()
-    process1.multiprocessing()
-    process2 = Munu()
-    process2.multiprocessing()
-    # process3 = EnormousBookCorpus()
-    # process3.multiprocessing()
+    # Done
+    # process1 = Malmunchi_book()
+    # process1.multiprocessing()
+
+    # Done
+    # process2 = Munu()
+    # process2.multiprocessing()
+
+    # Done
+    process3 = EnormousBookCorpus()
+    process3.multiprocessing()
+
     # process4 = Expertise()
     # print(process4)
     # process4.multiprocessing()
@@ -263,3 +266,6 @@ if __name__ == "__main__":
     # process5.multiprocessing()
     # process6 = NAMU()
     # process6.multiprocessing()
+
+    process7 = Essay()
+    process7.multiprocessing()
---
+++ scripts/text_preprocessing.py
@@ -0,0 +1,49 @@
+import re
+import regex
+from dataclasses import dataclass
+
+@dataclass
+class TextPreprocessing():
+    # Replace unneeded Data with mapping marks.
+    @staticmethod
+    def replace(text):
+        replace_mark = {'\\n': "\n", "\\'": "'", "‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": "sqrt", "×": "x", "²": "2",
+                 "—": "-", "–": "-", "’": "'", "_": "-", "`": "'", '“': '"', '”': '"', '“': '"', "£": "e",
+                 '∞': '무한', 'θ': '세타', '÷': '/', 'α': '알파', '•': '.', 'à': 'a', '−': '-', 'β': '베타',
+                 '∅': '', '³': '3', 'π': '파이', '\u200b': '', '…': '.', '\ufeff': '', 'करना': '', 'है': ''}
+
+        for mark in replace_mark:
+            text = text.replace(mark, replace_mark[mark])
+
+    @staticmethod
+    def sub(text):
+        # 이메일 제거
+        email_pattern = '([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'  # E-mail제거
+        text = re.sub(email_pattern, "", text)
+
+        # 링크 제거
+        link_pattern = '(http|ftp|https)://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'  # URL제거
+        text = re.sub(link_pattern, "", text)
+
+        # 일본어 한자 제거
+        japanese_pattern = r"[\p{Script=Hiragana}\p{Script=Katakana}\p{Script=Han}]"
+        text = regex.sub(japanese_pattern, "", text)
+
+        # 특수 문자 제거
+        special_word_pattern = '[-=+,#/\:^$.@*\"※~&%ㆍ』\\‘|\(\)\[\]\<\>`\'…》]'
+        text = re.sub(special_word_pattern, "", text)
+
+        # 줄바꿈 문자 제거
+        text = re.sub('\n', '', text)
+
+        # 연속된 공백 제거
+        text = re.sub(r"\s+", " ", text)
+
+        return text
+
+    @staticmethod
+    def preprocess(text):
+        text = TextPreprocessing.replace(text)
+        text = TextPreprocessing.sub(text)
+
+        return text
